a822ae1f99d10ba5a76a6c4c4e3e14cf201cf4ba
Aditya Nath Pal <145756795+adityapal861@users.noreply.github.com>
2023-09-27 17:47:08 +0530

    Update chat.js

--- models/chat.js
+++ models/chat.js
@@ -1,1 +1,24 @@
 const mongoose=require("mongoose")
+//Schema
+const chatSchema=new mongoose.Schema({
+    from:{
+        type:String ,
+        required:true
+    },
+    to:{
+        type:String,
+        required:true
+    },
+    msg:{
+        type:String,
+        maxLength:50
+    },
+    created_at:{
+        type:Date,
+        required:true,
+    }
+
+})
+//Model
+const Chat=mongoose.model("Chat",chatSchema);
+module.exports=Chat;  
